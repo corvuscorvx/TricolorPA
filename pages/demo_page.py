3587a8dd4ae7ddba2b7790ec28918954504aa32b
DrozdovMD <corvuscorvx@hotmail.com>
2026-09-23 19:10:18 +0300

    Added integration with Allure Refactoring conftest.py add attach.py add pytest.ini

--- pages/demo_page.py
+++ pages/demo_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 
 from pages.base_page import BasePage
@@ -33,6 +34,7 @@ class DemoPage(BasePage):
 
     ONLINE_CINEMA_BUTTON = (By.CSS_SELECTOR, '.header ._online-kino a')
 
+    @allure.step("Accept all cookies")
     def accept_cookies(self):
         self.wait_for_element_visible(self.COOKIES_POPUP)
         self.click_element(self.COOKIES_POPUP_BUTTON)
@@ -47,6 +49,7 @@ class DemoPage(BasePage):
         """Локатор области"""
         return By.XPATH, f"//div[contains(@class, 'geo-popup')]//a[text()='{region_name}']"
 
+    @allure.step("Pick geolocation {district_name} {region_name}")
     def select_geo_location(self, district_name: str, region_name: str):
         """Выбрать гео"""
         self.click_element(self.GEO_POPUP_WHITE_BUTTON)
@@ -61,71 +64,87 @@ class DemoPage(BasePage):
 
         self.wait_for_element_invisible(self.GEO_POPUP_WINDOW)
 
+    @allure.step("Get result text geolocation")
     def get_current_geo_text(self) -> str:
         """Получить текст гео из хэдэра"""
         return self.get_element_text(self.GEO_BUTTON_HEADER).strip()
 
+    @allure.step("Open the help page")
     def open_the_help_page(self):
         """Открыть страницу "Помощь" """
         self.click_element(self.HELP_BUTTON_HEADER)
 
+    @allure.step("Open the chat")
     def open_the_chat(self):
         """Открыть чат"""
         self.click_element(self.CHAT_BUTTON)
 
+    @allure.step("Enter text {text}")
     def write_message(self, text: str):
         """Написать сообщение в чат"""
         self.wait_for_element_visible(self.CHAT_WINDOW)
         self.send_keys_to_element(self.CHAT_FIELD, text)
         self.click_element(self.CHAT_SEND_BUTTON)
 
+    @allure.step("View sent message")
     def get_current_chat_message(self) -> str:
         """Получить текст сообщения из чата"""
         return self.get_element_text(self.CHAT_WINDOW)
 
+    @allure.step("Close the geolocation popup")
     def close_geo_popup(self):
         """Закрыть попап гео"""
         self.click_element(self.GEO_POPUP_BLUE_BUTTON)
 
+    @allure.step("Open search field")
     def open_search(self):
         """Открыть поле поиска из хэдэра"""
         self.click_element(self.SEARCH_BUTTON_HEADER)
 
-    def input_search_field(self, request):
+    @allure.step("Enter a search query {text}")
+    def input_search_field(self, text):
         """Ввести поискаовый запрос"""
         self.wait_for_element_visible(self.SEARCH_SHADE)
-        self.send_keys_to_element(self.SEARCH_FIELD, request)
+        self.send_keys_to_element(self.SEARCH_FIELD, text)
 
+    @allure.step("Click search button")
     def click_search_button(self):
         """Нажать кнопку поиска запроса"""
         self.click_element(self.SEARCH_BUTTON)
         self.wait_for_element_invisible(self.SEARCH_SHADE)
 
+    @allure.step("View search results")
     def get_current_search_result(self):
         """Получить текст результатов поиска"""
         return self.get_element_text(self.SEARCH_RESULT_AREA)
 
+    @allure.step("Click the 'Check subscriptions' button")
     def click_check_subscription_button(self):
         """Нажать кнопку "Проверить подписки" """
         self.scroll_to_element(self.CHECK_ID_AREA)
         self.click_element(self.CHECKING_SUBSCRIPTION_BUTTON)
 
+    @allure.step("Fill id filed with {number_id}")
     def input_id(self, number_id: int):
         """Ввести id для проверки"""
         self.send_keys_to_element(self.FIELD_ID, number_id)
 
+    @allure.step("Click the check button")
     def click_check_button(self):
         """Нажать кнопку првоерки"""
         self.click_element(self.CHECK_BUTTON)
 
+    @allure.step("Get error message")
     def get_error_invalid_id(self):
         """Получить текст ошибки првоерки id"""
         return self.get_element_text(self.ERROR_AREA)
 
+    @allure.step("Open oline cinema")
     def open_online_cinema(self):
         """Открыть онлайн-кинотеатр"""
         self.click_element(self.ONLINE_CINEMA_BUTTON)
 
+    @allure.step("Waiting for valid URL")
     def wait_for_online_cinema_url(self, url_part):
         """Ожидание урла онлайн-кинотеатра"""
         self.wait_for_url_contains(url_part)
--- tests/tests_demo.py
+++ tests/tests_demo.py
@@ -1,55 +1,79 @@
+import allure
+
+
 def test_change_geo(demo_page):
     district_name = "Северо-запад"
     region_name = "Санкт-Петербург и Ленинградская Область"
 
-    demo_page.accept_cookies()
-    demo_page.select_geo_location(district_name, region_name)
+    with allure.step("Accept all cookies"):
+        demo_page.accept_cookies()
 
-    actual_geo = demo_page.get_current_geo_text()
-    assert region_name in actual_geo, f"Отображается {region_name}"
+    with allure.step("Geolocation selection"):
+        demo_page.select_geo_location(district_name, region_name)
+
+    with allure.step("Geolocation check"):
+        actual_geo = demo_page.get_current_geo_text()
+        assert region_name in actual_geo, f"Отображается {region_name}"
 
 
 def test_write_in_the_chat(demo_page):
     message = "Это тест."
 
-    demo_page.accept_cookies()
-    demo_page.open_the_help_page()
-    demo_page.open_the_chat()
-    demo_page.write_message(message)
+    with allure.step("Accept all cookies"):
+        demo_page.accept_cookies()
 
-    actual_message = demo_page.get_current_chat_message()
-    assert message in actual_message, f"Отображается {message}"
+    with allure.step("Sending a message to the chat"):
+        demo_page.open_the_help_page()
+        demo_page.open_the_chat()
+        demo_page.write_message(message)
+
+    with allure.step("Displaying message"):
+        actual_message = demo_page.get_current_chat_message()
+        assert message in actual_message, f"Отображается {message}"
 
 
 def test_search(demo_page):
-    request = "кино"
-    demo_page.close_geo_popup()
-    demo_page.open_search()
-    demo_page.input_search_field(request)
-    demo_page.click_search_button()
+    text = "кино"
 
-    actual_result = demo_page.get_current_search_result()
-    assert request in actual_result, f"Отображается {request}"
+    with allure.step("Closing the geolocation popup"):
+        demo_page.close_geo_popup()
 
+    with allure.step("Search"):
+        demo_page.open_search()
+        demo_page.input_search_field(text)
+        demo_page.click_search_button()
 
-# def test_open_online_cinema(demo_page):
+    with allure.step("Checking search results"):
+        actual_result = demo_page.get_current_search_result()
+        assert text in actual_result, f"Отображается {text}"
+
 
 def test_negative_check_subscriptions_by_id(demo_page):
     number_id = 12345678912340
     error_text = "Неверный ID"
-    demo_page.accept_cookies()
-    demo_page.close_geo_popup()
-    demo_page.click_check_subscription_button()
-    demo_page.input_id(number_id)
-    demo_page.click_check_button()
 
-    actual_error = demo_page.get_error_invalid_id()
-    assert error_text in actual_error
+    with allure.step("Accept all cookies"):
+        demo_page.accept_cookies()
+
+    with allure.step("Closing the geolocation popup"):
+        demo_page.close_geo_popup()
+
+    with allure.step("Checking subscriptions by id"):
+        demo_page.click_check_subscription_button()
+        demo_page.input_id(number_id)
+        demo_page.click_check_button()
+
+    with allure.step("Check of result"):
+        actual_error = demo_page.get_error_invalid_id()
+        assert error_text in actual_error
 
 
 def test_redirect_to_online_cinema(demo_page):
     url_part = "kino.tricolor.ru"
-    demo_page.open_online_cinema()
-    demo_page.wait_for_online_cinema_url(url_part)
 
-    assert url_part in demo_page.driver.current_url, f"Ожидали '{url_part}' в текущем URL, но получили '{demo_page.driver.current_url}'"
+    with allure.step("Go to the online cinema"):
+        demo_page.open_online_cinema()
+        demo_page.wait_for_online_cinema_url(url_part)
+
+    with allure.step("URL check"):
+        assert url_part in demo_page.driver.current_url, f"Ожидали '{url_part}' в текущем URL, но получили '{demo_page.driver.current_url}'"
